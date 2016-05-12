6a60ddf652b232e840836ae4e7c9cf42b03c1025
Dominic Gannaway <dg@domgan.com>
2016-05-13 00:22:08 +0100

    progress made regarding mounting #3

--- src/shared.ts
+++ src/shared.ts
@@ -1,4 +1,4 @@
-export type Input = string | number | VElement | VTextNode | VComponent | VElement | VTemplate | Array<string | number | VElement | VTextNode | VComponent | VElement | VTemplate | Array<any>>;
+export type Input = string | number | VElement | VTextNode | VComponent | VTemplate | Array<string | number | VElement | VTextNode | VComponent | VTemplate | Array<any>>;
 
 export interface VElement {
 	_tag: string,
